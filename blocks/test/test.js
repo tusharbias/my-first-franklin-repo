af6425ca97a93fa93ca8adf7bc74d18a55363972
tushar <tushargu@adobe.com>
2023-05-16 15:37:02 +0530

    Update test.js

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -2,13 +2,11 @@ import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
   /* change to ul, li */
-  
-  console.log('tushar block -->' + block);
-  
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
+	console.log('Tushar -->' + row.innerHTML);
     [...li.children].forEach((div) => {
 		//console.log('Tushar -->' + row.innerHTML);
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'test-card-image';
