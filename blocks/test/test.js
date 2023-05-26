62722300f135c8889df5d02765900faf58d2ac78
tushar <tushargu@adobe.com>
2023-05-26 15:06:05 +0530

    Update test.js

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -17,7 +17,7 @@ export default function decorate(block) {
   });
   
   
-  
+  console.log('Tushar -->' + row.innerHTML);
   
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
