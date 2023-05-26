8a557531a0e5fd0183c295e659a7be3422ca3e0d
tushar <tushargu@adobe.com>
2023-05-26 15:16:07 +0530

    Update test.js

--- blocks/test/test.js
+++ blocks/test/test.js
@@ -8,14 +8,13 @@ export default function decorate(block) {
     li.innerHTML = row.innerHTML;
     console.log('Tushar -->', row.innerHTML);
     [...li.children].forEach((div) => {
-    //console.log('Tushar -->' + row.innerHTML);
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'test-card-image';
       else if (div.querySelector('h2')) div.className = 'test-heading';
-	  else div.className = 'test-card-body-content';
+      else div.className = 'test-card-body-content';
     });
     ul.append(li);
   });
-  
+
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
   block.append(ul);
